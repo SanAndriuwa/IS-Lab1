4dd79550404a6321d965e29e1630f96fb109b75e
SanAndriuwa <37011046+SanAndriuwa@users.noreply.github.com>
2026-09-09 21:16:09 +0300

    Clarify color feature extraction and handle empty masks

--- main/spalva_color.m
+++ main/spalva_color.m
@@ -1,33 +1,17 @@
-function hsv_value=spalva_color(Im)
-% im - vaizdas, nuskaitytas su imread
-% bw - binarinis vaizdas (nurodo sritá, kurioje yra skaièiuojamas 
-% spalvos vidutinë reikðmë)
-% atsakymas hsv_value - spalva HSV skalëje
-% 
+function hsv_value = spalva_color(Im)
+% Return the mean hue of the detected object.
+% Im is an RGB image read with imread.
+% The mask selects the object pixels used for the color feature.
 
-% index=bw(bw>0);
-% 
-% R=im(:,:,1);
-% meanR1=mean(mean(R(index)));
-% 
-% G=im(:,:,2);
-% meanG=mean(mean(G(index)));
-% 
-% B=im(:,:,3);
-% meanB=mean(mean(B(index)));
-% 
-% hsv_im=rgb2hsv(im);
-% hsv=hsv_im(:,:,1);
-% hsv_value=mean(mean(hsv(index)));
+BW = im2bw(rgb2gray(Im), 0.95);
+BW = imfill(~BW, 'holes');
+BW = imopen(BW, strel('disk', 12));
 
+if ~any(BW(:))
+    hsv_value = NaN;
+    return;
+end
 
-
-BW = im2bw(rgb2gray(Im),0.95);
-BW = imfill(~BW,'holes');
-BW = imopen(BW,strel('disk',12));
-
-
-hsv_im=rgb2hsv(Im);
-hsv=hsv_im(:,:,1);
-
-hsv_value=mean(mean(hsv(BW)));
+hsvImage = rgb2hsv(Im);
+hue = hsvImage(:, :, 1);
+hsv_value = mean(hue(BW));
